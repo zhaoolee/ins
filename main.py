6196fde7e62e51f77be445e00a134aaa85463666
zhaoolee <zhaoolee@gmail.com>
2022-11-13 21:31:04 +0800

    ins!

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ for website_info_index, website_info_row in website_info_data.iterrows():
         print("finish", website_info_row['Url'], website_info_row['Name'])
 website_info_data = website_info_data.drop(columns=['Url'])
 website_info_md = website_info_data.to_markdown(index=False)
-print(website_info_md)
+print(website_info_md,"len==", )
 readme_md = ""
 with open(os.path.join(os.getcwd(),"EditREADME.md"),'r') as load_f:
     readme_md = load_f.read();
@@ -33,7 +33,7 @@ mail_re = r'--insStart----insEnd--'
 reResult = re.findall(mail_re, readme_md)
 
 in_datetime = datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S')
-new_read_me = readme_md.replace(reResult[0], "\n\n" + "## 开源灵感库更新时间("+ in_datetime + ")\n\n" + website_info_md + "\n\n")
+new_read_me = readme_md.replace(reResult[0], "\n\n" + "## 开源灵感库已收录" + str(len(website_info_data)) + "个灵感!" + "更新时间("+ in_datetime + ")\n\n" + website_info_md + "\n\n")
 print('new_read_me',new_read_me)
 
 with open(os.path.join(os.getcwd(),"README.md"),'w') as load_f:
