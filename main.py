9744e86913f7daee7333e8adceca7b58821045c8
zhaoolee <zhaoolee@gmail.com>
2022-11-14 11:30:18 +0800

    ins!

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ import requests
 website_info_data = pd.read_csv('./website_info.csv')
 website_info_data = website_info_data.reindex(index=website_info_data.index[::-1])
 print(website_info_data)
-
-
-
-
 for website_info_index, website_info_row in website_info_data.iterrows():
     print('=start=>>', website_info_index, website_info_row['Url'])
     try:
@@ -29,16 +25,11 @@ for website_info_index, website_info_row in website_info_data.iterrows():
         website_info_row['Name'] = "<span>" + website_info_row['Name'] + "</span>" + "<a href='" + website_info_row['Url'] + "'>" + (website_info_row['Url'] if len(website_info_row['Url']) < 30 else website_info_row['Url'][0:30] + "..." ) + "</a>"
         print("finish", website_info_row['Url'], website_info_row['Name'])
 website_info_data = website_info_data.drop(columns=['Url'])
-
 website_info_html = "<table>"
-
 website_info_html = website_info_html + "<tr>" + "<td width='400'>" + "<span>(づ｡◕‿‿◕｡)づ</span><br/><span>Name</span>" + "</td>"  + "<td>" + "<span> (●ﾟωﾟ●)</span><br/><span>Description</span>"  + "</td>"  + "<td width='300'>" + "<span> ︿(￣︶￣)︿</span><br/><span>Tag</span>"  + "</td>"  +"</tr>"
-
 for website_info_index, website_info_row in website_info_data.iterrows():
     website_info_html = website_info_html + "<tr>" + "<td>" + website_info_row['Name'] + "</td>"  + "<td>" + website_info_row['Description'] + "</td>"  + "<td>" + website_info_row['Tag'] + "</td>"  +"</tr>"
-
 website_info_html = website_info_html + "</table>"
-
 website_info_md = website_info_data.to_markdown(index=False)
 print(website_info_md, website_info_html)
 readme_md = ""
@@ -46,12 +37,9 @@ with open(os.path.join(os.getcwd(),"EditREADME.md"),'r') as load_f:
     readme_md = load_f.read();
 mail_re = r'--insStart----insEnd--'
 reResult = re.findall(mail_re, readme_md)
-
 in_datetime = datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S')
 # new_read_me = readme_md.replace(reResult[0], "\n\n" + "## 开源灵感库已收录" + str(len(website_info_data)) + "个灵感!" + "更新时间("+ in_datetime + ")\n\n" + website_info_md + "\n\n")
 new_read_me = readme_md.replace(reResult[0], "\n\n" + "## 开源灵感库已收录" + str(len(website_info_data)) + "个灵感!" + "(～￣▽￣)～更新时间("+ in_datetime + ")\n\n" + website_info_html + "\n\n")
-
 print('new_read_me',new_read_me)
-
 with open(os.path.join(os.getcwd(),"README.md"),'w') as load_f:
     load_f.write(new_read_me)
